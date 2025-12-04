57770b752c2d55f6719df16179e98686d46bd987
EmoSavage16 <emirhanfiliz703@gmail.com>
2025-12-04 19:45:26 +0300

    Update api.ts

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:4000/api";
+const API_BASE = "http://35.189.234.33:4000/api";
 
 export async function login(email: string, password: string) {
   const res = await fetch(`${API_BASE}/auth/login`, {
